011ab973120a0c6089db170a152b11d8c99ea12d
Leonard Hariton <leohariton@gmail.com>
2024-05-12 19:31:33 +0300

    progress

--- main.gd
+++ main.gd
@@ -3,7 +3,8 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#Utils.saveGame()
+	Utils.loadGame()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
